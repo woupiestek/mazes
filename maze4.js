053fd94d29f22c2c617b8ec682551f1145f23d5c
Wouter Pieter Stekelenburg <woupiestek@yahoo.com>
2024-05-24 23:31:25 +0200

    New plan for maze 4

--- maze4.js
+++ maze4.js
@@ -1,90 +1,108 @@
+function sample(array) {
+  if (!array?.length) {
+    return undefined;
+  }
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 function walk({ maxX, maxY }) {
   const weights = [];
-  const nodes = [];
-  for (let x = maxX - 1; x >= 0; x--) {
+  for (let x = maxX; x >= 0; x--) {
     weights[x] = [];
-    for (let y = maxY - 1; y >= 0; y--) {
-      weights[x][y] = Math.random();
-      nodes.push([x, y]);
+    for (let y = maxY; y >= 0; y--) {
+      weights[x][y] = 0;
     }
   }
-  nodes.sort((a, b) => weights[a[0]][a[1]] - weights[b[0]][b[1]]);
   const walls = [];
+  const nodes = [
+    [0, 0],
+    [0, maxY],
+    [maxX, 0],
+    [maxX, maxY],
+  ];
+  weights[0][0] = 2;
+  weights[1][0] = 1;
+  weights[0][1] = 1;
+
+  weights[0][maxY] = 2;
+  weights[1][maxY] = 1;
+  weights[0][maxY - 1] = 1;
+
+  weights[maxX][0] = 2;
+  weights[maxX - 1][0] = 1;
+  weights[maxX][1] = 1;
+
+  weights[maxX][maxY] = 2;
+  weights[maxX - 1][maxY] = 1;
+  weights[maxX][maxY - 1] = 1;
+
   a: while (nodes.length > 0) {
-    let [x, y] = nodes.pop();
-    if (typeof weights[x][y] !== "number") continue;
-    let node = [x, y];
-    const wall = [];
+    const i = Math.floor(Math.random() * nodes.length);
+    const node = nodes[i];
+    if (i !== nodes[length - 1]) {
+      nodes[i] = nodes[nodes.length - 1];
+      nodes[nodes[length - 1]] = node;
+    }
+    const wall = [node];
+    let [x, y] = node;
+
     for (;;) {
-      if (
-        weights[x-1] &&
-        typeof weights[x - 1][y] === "number" &&
-        weights[x - 1][y] > weights[node[0]][node[1]]
-      )
-        node = [x - 1, y];
-      if (
-        typeof weights[x][y - 1] === "number" &&
-        weights[x][y - 1] > weights[node[0]][node[1]]
-      ) {
-        node = [x, y - 1];
-      }
-      if (
-        weights[x+1] &&
-        typeof weights[x + 1][y] === "number" &&
-        weights[x + 1][y] > weights[node[0]][node[1]]
-      ) {
-        node = [x + 1, y];
-      }
-      if (
-        typeof weights[x][y + 1] === "number" &&
-        weights[x][y + 1] > weights[node[0]][node[1]]
-      ) {
-        node = [x, y + 1];
-      }
-      //weights[x][y] = null;
-      wall.push([x, y]);
-      if (node[0] === x && node[1] === y) {
-        // reach a low point, so give up;
-        if (wall.length>1) {
+      const next = sample(
+        [
+          [x - 1, y],
+          [x, y - 1],
+          [x + 1, y],
+          [x, y + 1],
+        ].filter(([i, j]) => weights[i]?.[j] <= 1)
+      );
+      if (!next) {
+        nodes.pop();
+        if (wall.length > 1) {
           walls.push(wall);
         }
         continue a;
       }
-      [x, y] = node;
+      weights[next[0]][next[1]] = 2;
+      if (weights[next[0] - 1]?.[next[1]] <= 1) weights[next[0] - 1][next[1]]++;
+      if (weights[next[0]]?.[next[1] - 1] <= 1) weights[next[0]][next[1] - 1]++;
+      if (weights[next[0] + 1]?.[next[1]] <= 1) weights[next[0] + 1][next[1]]++;
+      if (weights[next[0]]?.[next[1] + 1] <= 1) weights[next[0]][next[1] + 1]++;
+      wall.push(next);
+      nodes.push(next);
+      [x, y] = next;
     }
   }
   return walls;
 }
 
 function draw(canvas, { maxX, maxY, unit }, walls) {
-    const context = canvas.getContext("2d");
-  
-    canvas.setAttribute("width", maxX * unit + 2);
-    canvas.setAttribute("height", maxY * unit + 2);
-    context.fillStyle = "#000000";
-    context.lineCap = "round";
-    context.lineJoin = "round";
-  
-    for (let i = 0, l = walls.length; i < l; i++) {
-      const [[x0, y0], ...t] = walls[i];
-      if (t.length > 0) {
-        context.beginPath();
-        context.moveTo(x0 * unit + 1, y0 * unit + 1);
-        for (const p of t) {
-          context.lineTo(p[0] * unit + 1, p[1] * unit + 1);
-        }
-        context.stroke();
+  const context = canvas.getContext("2d");
+
+  canvas.setAttribute("width", maxX * unit + 2);
+  canvas.setAttribute("height", maxY * unit + 2);
+  context.fillStyle = "#000000";
+  context.lineCap = "round";
+  context.lineJoin = "round";
+
+  for (let i = 0, l = walls.length; i < l; i++) {
+    const [[x0, y0], ...t] = walls[i];
+    if (t.length > 0) {
+      context.beginPath();
+      context.moveTo(x0 * unit + 1, y0 * unit + 1);
+      for (const p of t) {
+        context.lineTo(p[0] * unit + 1, p[1] * unit + 1);
       }
+      context.stroke();
     }
   }
-  
-  const Config = {
-    maxX: 69,
-    maxY: 69,
-    unit: 10,
-  };
-  
-  export function run(canvas) {
-    draw(canvas, Config, walk(Config));
-  }
-  
+}
+
+const Config = {
+  maxX: 69,
+  maxY: 69,
+  unit: 10,
+};
+
+export function run(canvas) {
+  draw(canvas, Config, walk(Config));
+}
